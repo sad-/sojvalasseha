a6f5f9bf9b725f1829e300d28737647eba685c05
Meha Bakshi <mehabakshi@Mehas-MacBook-Air.local>
2016-05-02 10:49:36 -0700

    putting back what ghassan had

--- weightCycles.py
+++ weightCycles.py
@@ -15,13 +15,32 @@ from parse_inputs import parseInput
       if len(argv) != 2:
          print("Usage Error, Weiht Cycles expects a .in and a .cyc file")
       else:
+         inFile, cycFile = argv
+         children = readIn(inFile)
+         cycList = readCyc(cycFile)
+         cycleWeights = weightCyc(cycList)
+
 
    def readIn(fileName):
       """ Return children from original input file"""
       children = parseInput(fileName, child = True)
 
    def readCyc(fileName):
-      return cycleList = [map(int, line.split() for line in open(fileName)]
+      return [map(int, line.split() for line in open(fileName))] 
 
    def weightCyc(children, cycList):
+
+
+   def selectCyc(cycles, cycleWeight):
+      """Need to select cycles 
+      which do not share vertices 
+      Independent set or SCC
+      First approach: greedily add cycles 
+      """
+
+
+      selectedVertices = set()
+
+      weights = np.array(cycleWeight)
+      sortedIndices = np.argsort()
       
