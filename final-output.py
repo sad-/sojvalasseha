d5d10c01e2f048e30fc0f396d1725d21447200c3
Meha Bakshi <mehabakshi@Mehas-MacBook-Air.local>
2016-05-02 22:28:32 -0700

    updated final-output

--- final-output.py
+++ final-output.py
@@ -3,11 +3,16 @@
 
 out = open('solutions.out', 'w')
 for i in range(1,493):
-	s = 'final/' + str(i) + '.out'
-	f = open(s, 'r')
-	if not f:
-		continue
-	curr = ''
-	for line in f:
-		curr += line[:-1] + '; '
-	out.write(curr + '\n')
+	try: 
+		s = 'final/' + str(i) + '.out'
+		f = open(s, 'r')
+		if not f:
+			continue
+		curr = ''
+		for line in f:
+			curr += line[:-1] + '; '
+		out.write(curr + '\n')
+	except FileNotFoundError:
+		print("File #" + str(i) + " not found")
+		break
+
